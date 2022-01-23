905b55c67244368218b9547dd7cf7eecd4234dfd
Andrey Gribanov <shade1471@gmail.com>
2022-01-23 14:23:06 +0700

    Add method with sort by time and tests for it

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -4,6 +4,7 @@ import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
     private TicketRepository repository;
@@ -16,6 +17,8 @@ public class TicketManager {
         repository.save(ticket);
     }
 
+    // Метод поиска с сортировкой по цене
+
     public Ticket[] findByCondition(String from, String to) {
         Ticket[] result = new Ticket[0];
         for (Ticket ticket : repository.findAll()) {
@@ -27,6 +30,23 @@ public class TicketManager {
             }
         }
         Arrays.sort(result);
+        return result;
+    }
+
+    // Метод поиска с сортировкой по времени полета
+
+    public Ticket[] findByCondition(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        comparator = new TicketByTravelTimeAscComparator();
+        for (Ticket ticket : repository.findAll()) {
+            if (ticket.matches(from, to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, comparator);
         return result;
     }
 }
